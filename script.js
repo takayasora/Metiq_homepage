f13a54697c34083044bb6a088b7de5fbe2bc953f
Tsora1216 <so12ra16@icloud.com>
2023-03-23 02:01:07 +0900

    error update

--- script.js
+++ script.js
@@ -1,20 +1,21 @@
-window.onload = ()=>  {
-    alert("Enter Username and Password for Registration.");
-    let uname = prompt("Enter Username")
-    let pass = prompt("Enter Password")
-    let ubox = document.getElementById("usern")
-    let pbox = document.getElementById("pass")
+window.onload = () => {
+  alert("Enter Username and Password for Registration.");
+  let uname = prompt("Enter Username")
+  let pass = prompt("Enter Password")
+  let ubox = document.getElementById("usern")
+  let pbox = document.getElementById("pass")
 
-    let form= document.getElementById("formw")
-    form.addEventListener('submit', function(e) {
-        e.preventDefault();
-        if(uname === ubox.value && pass=== pbox.value) {
-            alert("validated")
-          location.assign("./loggedin/index.html")
-        }
-        else {
-            alert("wrong password")
-        }
+  let form = document.getElementById("formw")
+  form.addEventListener('submit', function (e) {
+    e.preventDefault();
+    if (uname === ubox.value && pass === pbox.value) {
+      alert("validated")
+      location.assign("./loggedin/index.html")
+    }
+    else {
+      alert("wrong password")
+      location.assign("./error/index.html")
+    }
 
-    })
-  };
+  })
+};
