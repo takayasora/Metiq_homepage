28114f1637b8fa093a4713ddead8fc41ee7f3d23
Tsora1216 <so12ra16@icloud.com>
2023-03-23 04:03:02 +0900

    update link

--- download/aaa/script.js
+++ download/aaa/script.js
@@ -1,11 +1,11 @@
 function downloadFile() {
   // ファイルのURL
-  var url = 'download.zip';
+  var url = 'https://metiq.co/download/aaa/download.zip';
   
   // 一時的なリンクを作成し、クリックしてダウンロードする
   var link = document.createElement('a');
   link.href = url;
-  link.download = url.split('/').pop();
+  link.download = 'download.zip';
   document.body.appendChild(link);
   link.click();
   document.body.removeChild(link);
