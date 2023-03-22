024e751ec3a4de6d77fdb3306abbd1bb4792d048
Tsora1216 <so12ra16@icloud.com>
2023-03-23 03:17:49 +0900

    update pass

--- index.js
+++ index.js
@@ -2,13 +2,13 @@ window.onload = () => {
   alert("Enter Username and Password for Registration.");
   // let uname = prompt("Enter Username")
   // let pass = prompt("Enter Password")
-  // let ubox = document.getElementById("usern")
-  // let pbox = document.getElementById("pass")
+  let ubox = document.getElementById("usern")
+  let pbox = document.getElementById("pass")
 
   let form = document.getElementById("formw")
   form.addEventListener('submit', function (e) {
     e.preventDefault();
-    if (uname === ubox.value && pass === pbox.value) {
+    if ("aaa" === ubox.value && "bbb" === pbox.value) {
       alert("validated")
       location.assign("./loggedin/index.html")
     }
