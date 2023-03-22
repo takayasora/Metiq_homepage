310c197d9483c867e59a276896bcff91aeed6212
Tsora1216 <so12ra16@icloud.com>
2023-03-23 03:03:52 +0900

    update system

--- index.js
+++ index.js
@@ -1,17 +1,13 @@
 window.onload = () => {
-  alert("Enter Username and Password for Registration.");
-  let uname = prompt("Enter Username")
-  let pass = prompt("Enter Password")
   let ubox = document.getElementById("usern")
   let pbox = document.getElementById("pass")
 
   let form = document.getElementById("formw")
   form.addEventListener('submit', function (e) {
     e.preventDefault();
-    if (uname === ubox.value && pass === pbox.value) {
+    if (uname === "20ec070@ms.dendai.ac.jp" && pass === "nameko1222") {
       alert("validated")
       location.assign("./loggedin/index.html")
-
     }
     else {
       alert("wrong password")
