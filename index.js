547947b2a565dc12537db117f367b8a5bc43adff
Tsora1216 <so12ra16@icloud.com>
2023-03-23 02:59:55 +0900

    update tree

---
+++ index.js
@@ -0,0 +1,24 @@
+window.onload = () => {
+  alert("Enter Username and Password for Registration.");
+  let uname = prompt("Enter Username")
+  let pass = prompt("Enter Password")
+  let ubox = document.getElementById("usern")
+  let pbox = document.getElementById("pass")
+
+  let form = document.getElementById("formw")
+  form.addEventListener('submit', function (e) {
+    e.preventDefault();
+    if (uname === ubox.value && pass === pbox.value) {
+      alert("validated")
+      location.assign("./loggedin/index.html")
+
+    }
+    else {
+      alert("wrong password")
+      location.assign("./error/index.html")
+    }
+
+  })
+};
+
+
