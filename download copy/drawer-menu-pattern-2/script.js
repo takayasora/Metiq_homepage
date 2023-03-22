beb062bb14ce043fa96bec9928b8b0f91b058a7e
Tsora1216 <so12ra16@icloud.com>
2023-03-23 04:15:22 +0900

    update file

--- download copy/drawer-menu-pattern-2/script.js
+++ download copy/drawer-menu-pattern-2/script.js
@@ -1,16 +1,3 @@
-function downloadFile() {
-  // ファイルのURL
-  var url = 'https://metiq.co/download/download.zip';
-  
-  // 一時的なリンクを作成し、クリックしてダウンロードする
-  var link = document.createElement('a');
-  link.href = url;
-  link.download = 'download.zip';
-  document.body.appendChild(link);
-  link.click();
-  document.body.removeChild(link);
-};
-
 const attachEvent = (element, event, handler, options) => {
   element.addEventListener(event, handler, options);
   return {
